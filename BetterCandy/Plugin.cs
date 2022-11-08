3858f66f89c1d60a3bd5c623dd6a6f1cbf554480
TGLeoz <72647720+TGLeoz@users.noreply.github.com>
2022-11-08 19:46:26 +0700

    Update Plugin.cs

--- BetterCandy/Plugin.cs
+++ BetterCandy/Plugin.cs
@@ -1,35 +1,28 @@
-﻿using Exiled.API.Features;
+using Exiled.API.Features;
 using System;
-using System.Collections.Generic;
 using Player = Exiled.Events.Handlers.Player;
-using Server = Exiled.Events.Handlers.Server;
-namespace BetterCandy
+namespace BetterCandyPorted
 {
     public class Plugin : Plugin<Config>
     {
-        public override Version RequiredExiledVersion { get; } = new Version(5, 0, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(5, 2, 0);
         public override string Name { get; } = "BetterCandy";
-        public override Version Version { get; } = new Version(1, 1, 0);
+        public override Version Version { get; } = new Version(1, 0, 0);
 
-        public static Plugin Singleton;
         public Handlers.Player2 player;
 
         public override void OnEnabled()
         {
 
-            Singleton = this;
             player = new Handlers.Player2(this);
-
-            Player.InteractingScp330 += player.OnInteractingWithScp330;
-
+            Exiled.Events.Handlers.Scp330.InteractingScp330 += player.OnInteractingWithScp330;
             base.OnEnabled();
         }
 
         public override void OnDisabled()
         {
-            Player.InteractingScp330 -= player.OnInteractingWithScp330;
+            Exiled.Events.Handlers.Scp330.InteractingScp330 -= player.OnInteractingWithScp330;
 
-            Singleton = null;
             player = null;
 
             base.OnDisabled();
