11a916c5efb12730dc987634e28b4ec419f2109c
TGLeoz <72647720+TGLeoz@users.noreply.github.com>
2022-11-08 19:35:56 +0700

    Add files via upload

---
+++ BetterCandy/Handlers/Player2.cs
@@ -0,0 +1,34 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+namespace BetterCandy.Handlers
+{
+    public class Player2
+    {
+        private readonly Plugin plugin;
+        public Player2(Plugin plugin) => this.plugin = plugin;
+
+        public void OnInteractingWithScp330(InteractingScp330EventArgs ev)
+        {
+            if (ev.UsageCount == plugin.Config.PickCandyTimes)
+            {
+                ev.ShouldSever = true;
+            }
+            else
+            {
+                ev.ShouldSever = false;
+            }
+
+            int RandomNumber = Random.Range(1, Plugin.Singleton.Config.MaxRandomizer);
+
+            if (RandomNumber == plugin.Config.ChoosenNumber)
+            {
+                ev.IsAllowed = false;
+                ev.Player.TryAddCandy(InventorySystem.Items.Usables.Scp330.CandyKindID.Pink);
+                Debug.Log("Cu de anao pelado sem roupa sexooxoxoxoaxoasxosdaosdao");
+            }
+        }
+    }
+}
